7a1be118fa5542bcc306b887bb97a0c7ce789fe6
Sabarinathan3 <theresilientks@email.com>
2025-12-07 15:40:20 +0530

    fix: case conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ScrollProgress from "./components/ScrollProgress";
 import MainLayout from "./layouts/MainLayout";
-import Hero from "./sections/hero-temp.jsx";
+import Hero from "./sections/Hero.jsx";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
